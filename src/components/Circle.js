a4259e64a93d4a1a360aa454b01de4ae134b2dc5
Vladislav Efanov <vladislav.efanov@gmail.com>
2022-09-15 01:19:02 +0200

    feat: create Circle element

---
+++ src/components/Circle.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import stylesFlex from './stylesFlex';
+
+const Circle = styled.div`
+  ${stylesFlex};
+  width: 56px;
+  height: 56px;
+  background-color: #E7EBFC;
+  border-radius: 50%
+`;
+
+export default Circle;

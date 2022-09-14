202663271a6105fcf08e08c102aa71e214d71ccd
Vladislav Efanov <vladislav.efanov@gmail.com>
2022-09-15 01:20:35 +0200

    feat: create Wrapper cmp

---
+++ src/components/Wrapper.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.section`
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 0 50px;
+
+  @media (max-width: 992px) {
+    padding: 0 30px;
+  }
+`;
+
+export default Wrapper;
